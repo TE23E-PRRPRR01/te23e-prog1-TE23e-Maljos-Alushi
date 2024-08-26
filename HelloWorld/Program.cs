02acdc8b7d9deebe1f00fdbacfaabdf11b261806
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2024-08-26 14:31:53 +0200

    Tränat på cw, cr och svenska tecken

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.Clear();
+
+Console.WriteLine("Hej världen!");
+Console.WriteLine("nu mitt första c#-program");
+
+Console.Write("Vad heter du?");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej!" + namn);
+
+Console.BackgroundColor = ConsoleColor.DarkCyan
+Console.ForegroundColor = ConsoleColor.Gray
+Console.WriteLine("Såg du på os?");
+string gol = Console.ReadLine();
+Console.WriteLine("Va bra!" + gol);
+
+
+
+
+
+
---
+++ Kapitel-1/Registrering/Program.cs
@@ -0,0 +1,12 @@
+﻿// Program för att registrera förnamn och efternam 
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Program för att registrera förnamn och efternam" );
+Console.Write("Ange förnamn:");
+string förnamn = Console.ReadLine();
+Console.Write("Ange efternamn:");
+string efternamn = Console.ReadLine();
+Console.WriteLine("Hej! "+ förnamn + " " + efternamn);
+Console.WriteLine($"Hej! {förnamn} {efternamn}");
+

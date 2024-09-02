f76935062b148a02ebcd4e30c6faf218ff03e05b
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2024-09-02 14:54:03 +0200

    tränat på if satser

---
+++ Kapitel-3/EngChart/Program.cs
@@ -0,0 +1,64 @@
+﻿// Program som simulerar kända Engeneering Flowchart
+using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+
+Console.Clear();
+Console.WriteLine("Välkomen till Ingensjörens flödesschema");
+
+
+// ACSii art
+Console.WriteLine($"""
+                                  _
+                                    (_)
+              |    .
+          .   |L  /|   .          _
+      _ . |\ _| \--+._/| .       (_)
+     / ||\| Y J  )   / |/| ./
+    J  |)'( |        ` F`.'/        _
+  -<|  F         __     .-<        (_)
+    | /       .-'. `.  /-. L___       
+    J \      <    \  | | O\|.-'  _   
+  _J \  .-    \/ O | | \  |F    (_) 
+ '-F  -<_.     \   .-'  `-' L__    
+__J  _   _.     >-'  )._.   |-'  
+`-|.'   /_.           \_|   F    
+  /.-   .                _.<     
+ /'    /.'             .'  `\    
+  /L  /'   |/      _.-'-\
+ /'J       ___.---'\|
+   |\  .--' V  | `. `
+   |/`. `-.     `._)
+      / .-.\
+VK    \ (  `\
+       `.\
+""");
+
+Console.Write("Does it move? (y/n)");
+string answer = Console.ReadLine();
+//kolla om svar är ja eller nej 
+if (answer == "y")
+{ // y
+    Console.Write("Should it? (y/n)");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("No Problem!");
+    }
+    else
+    {
+        Console.WriteLine("Please use Duct Tape!");
+    }
+}
+else
+{// n
+    Console.Write("Should it? (y/n)");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("Please use WD-40!");
+    }
+    else
+    {
+        Console.WriteLine("No problem!");
+    }
+}
---
+++ Kapitel-3/InteractivStory/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+Console.Clear();
+
+Console.WriteLine("welcome till Drake och dongeunus.");
+
+Console.Write("You see a cave, do you go inside of it? (y/n)");
+string answer  = Console.ReadLine();
+
+if (answer=="y")
+{
+    Console.WriteLine("Now you are inside of the cave and you see a wizard. Do you meet him or leave hi alone? (y/n)");
+}
+else
+{
+    Console.WriteLine("You will never know what was inside!!");
+}

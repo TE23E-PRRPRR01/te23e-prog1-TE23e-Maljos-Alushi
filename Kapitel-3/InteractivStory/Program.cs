0752eafb21e35dfb03f93a4949d1af26ce079dc6
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2024-09-04 14:57:58 +0200

    stoy mode

--- Kapitel-3/InteractivStory/Program.cs
+++ Kapitel-3/InteractivStory/Program.cs
@@ -6,11 +6,31 @@ Console.WriteLine("welcome till Drake och dongeunus.");
 Console.Write("You see a cave, do you go inside of it? (y/n)");
 string answer  = Console.ReadLine();
 
-if (answer=="y")
+if (answer == "y")
 {
-    Console.WriteLine("Now you are inside of the cave and you see a wizard. Do you meet him or leave hi alone? (y/n)");
+    Console.WriteLine("Now you are inside of the cave and you see a wizard. Do you talk to him or leave him alone? (y/n)");
+    answer=Console.ReadLine();
+    
+    if (answer=="y")
+    {
+        Console.WriteLine("The wizard gives you a potion. Do you drink it? (y/n)");
+        answer = Console.ReadLine();
+    
+        if (answer=="y")
+        {
+            Console.WriteLine("You have been poisoned! and now you are dead.");
+        }
+        else
+        {
+            Console.WriteLine("The wizard gets mad at you and throws the potion at your head and you die.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("You go further in the cave and die from low oxygen levels!");
+    }
 }
 else
 {
-    Console.WriteLine("You will never know what was inside!!");
+    Console.WriteLine("You live a another day!");
 }

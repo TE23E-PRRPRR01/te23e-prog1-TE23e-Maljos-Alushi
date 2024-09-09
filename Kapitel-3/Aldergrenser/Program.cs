30d762c268992e112a562155c2ecc9f0c039bd4c
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2024-09-09 14:55:11 +0200

    tränat heltal och konvertering

---
+++ Kapitel-3/Aldergrenser/Program.cs
@@ -0,0 +1,29 @@
+﻿// Skapa ett program som implementerar valfria åldersregler.
+//Välj 4 åldersgränser och associera dem med något som är tillåtet att göra vid den åldern.
+Console.Clear();
+
+
+Console.WriteLine("Kan du dricka en ÖL i Europa?");
+
+Console.Write("Hur gammal är du?");
+
+string ageText = Console.ReadLine();
+
+int age = int.Parse(ageText);
+
+if (age >= 20)
+{
+    Console.WriteLine("Du får dricka öl i alla lände i Europa");
+}
+else if (age <= 17 && age >= 16)
+{ 
+    Console.WriteLine("Nä du fåt inte drika i de flesta europeiska länderna.");
+}
+else if (age == 15)
+{
+    Console.WriteLine("Du kan inte dricka Öl als förutom i privat område i de flästa länderna i europa.");
+}
+else if (age >= 14)
+{
+    Console.WriteLine("Nej du kan inte dricka alls");
+}
---
+++ Kapitel-3/GissaTalet/Program.cs
@@ -0,0 +1,29 @@
+﻿// Ett litet spel - gissa ett hemligt heltal!
+Console.Clear();
+Console.WriteLine("Ett litet spel - gissa ett hemligt heltal!");
+
+//slumpa ett tal
+int slumptalet = Random.Shared.Next(1, 101); // mellan 1-100
+Console.WriteLine($"Slumptalet är {slumptalet}");
+
+Console.Write("Gissa ett tal 1-100: ");
+// läs in texten, t.ex "100" 
+string gissningText = Console.ReadLine();
+// konvertera text till heltal, blir 100
+int gissning = int.Parse(gissningText);
+
+if (gissning == slumptalet)
+{
+    Console.WriteLine("Bra du har gissat rätt!");
+    
+}
+else if (gissning > slumptalet)
+{
+    Console.WriteLine("För Högt");
+}
+else
+{
+    Console.WriteLine("För Lågt");
+}
+
+

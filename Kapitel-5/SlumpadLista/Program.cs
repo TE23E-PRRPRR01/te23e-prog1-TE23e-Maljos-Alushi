efb0f8a8682c516aae2293fe8a0d81a017fba449
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2024-11-06 14:55:42 +0100

    break,loop, list

---
+++ Kapitel-5/SlumpadLista/Program.cs
@@ -0,0 +1,31 @@
+﻿// Slumpa många tal och spara i lista
+using System.ComponentModel.DataAnnotations;
+Console.Clear();
+Console.WriteLine("Slumpa många tal och spara i lista");
+
+List<int> listaSlumptal = [];
+
+// be användaren ange antal slumptal
+Console.Write("Ange antal slumptal: ");
+int antal = int.Parse(Console.ReadLine());
+
+// Be användaren ange min och max slumptal
+Console.Write("Ange minsta värde för slumptal: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Ange högsta värde för slumptal: ");
+int max = int.Parse(Console.ReadLine());
+// loppa 5 ggr 
+for (int i = 0; i< 5; i++)
+{        
+// slumpa ett tal 1-100
+int slumptal = 0;
+slumptal = Random.Shared.Next(min, max+1);
+Console.WriteLine($"Slumpat tal {i}: {slumptal}");
+
+// lägg till slumptalet i listan
+
+listaSlumptal.Add(slumptal);
+
+Console.WriteLine($"Slumpat tal");
+
+}

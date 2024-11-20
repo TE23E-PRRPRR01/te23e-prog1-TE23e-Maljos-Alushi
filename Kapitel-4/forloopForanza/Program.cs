8caddbd344ef58cc96f505845408552183488104
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2024-11-20 15:00:53 +0100

    for loop

---
+++ Kapitel-4/forloopForanza/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+Console.Clear();
+string input = Console.ReadLine();
+Console.Write("Ange minimum värde: ");
+int min = int.Parse(Console.ReadLine());
+
+Console.Write("Ange maximum värde: ");
+int max = int.Parse(Console.ReadLine());
+int summan = 0;
+for (int i = min; i <= max + 1; i++)
+{
+    if (i % 2 == 0)
+    {
+   summan += i;
+
+    }
+}
+Console.WriteLine($"Summan av sifruorna {min} till {max} är {summan}");
+
+Console.WriteLine("Jämna siffror från 1 till 20: ");
+for (int i = 1; i < 20; i++)
+{
+    if (1 % 2 == 0)
+    {
+        Console.Write($"{i}");
+    }
+}

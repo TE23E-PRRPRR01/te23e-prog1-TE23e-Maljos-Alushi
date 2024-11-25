c2c652a257bfb13856e973a357f1f98004dd7564
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2024-11-25 14:50:39 +0100

    tränat på lisort igen

---
+++ Kapitel-5/Microovningar1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+// listan
+List<string> listaNamn = ["Anna", "Björn", "Cecilia"];
+
+Console.Write("Nuvarande lista: ");
+
+Console.WriteLine(string.Join($",", listaNamn));
+
+Console.WriteLine("Ange ett namn att addera till listan:");
+string namn = Console.ReadLine();
+listaNamn.Add($"{namn}");
+
+Console.Write("Listan efter tillägg : ");
+
+Console.WriteLine(string.Join($",", listaNamn));
+
+Console.WriteLine("Ange ett namn att ta bort :");
+string bortNamn = Console.ReadLine();
+
+if (listaNamn.Contains(bortNamn))
+{
+    Console.WriteLine("Det finns");
+    listaNamn.Remove(bortNamn);
+}
+else
+{
+    Console.WriteLine("Det finns inte");
+}
+
+Console.WriteLine(string.Join($",", listaNamn));

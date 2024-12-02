596f02a4a8479d171b66725fe91f29ad4128812a
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2024-12-02 14:53:19 +0100

    list foreach

---
+++ Kapitel-5/MicroÖvningar 3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+
+//Skriv ut lista med foreach
+List<string> listaNamn = ["Pizza", "Pasta", "Sallad",];
+Console.WriteLine("Alla namn: ");
+foreach (string namn in listaNamn)
+{
+    Console.WriteLine($"- {namn}");
+}
+
+//Skriv ut alla årtalen i en lista
+List<int> listÅrtal = [1492, 1776, 1914, 1945, 1969];
+Console.WriteLine("Årtal i listan: ");
+foreach (int årtal in listÅrtal)
+{
+    Console.WriteLine($"* {årtal}");
+}
+
+//Skriv ut på en rad
+Console.Write("Alla årtal: ");
+foreach (int år in årtal)
+{
+    Console.Write($"{år}, ");
+}
+Console.WriteLine();
+

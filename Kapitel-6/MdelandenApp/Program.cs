ec65f15cd916adea1c170650f2b69fbcc729a2c7
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2025-01-22 15:05:48 +0100

    tränat på metoder och topdown

---
+++ Kapitel-6/MdelandenApp/Program.cs
@@ -0,0 +1,83 @@
+﻿/*************
+*** M A I N ***
+**************/
+
+
+Console.Clear();
+
+VisaPresentation();
+
+string filnamn = "Text.medd";
+
+while (true)
+{
+   VisaMeny();
+    string val = Console.ReadLine();
+    if (val == "1")
+    {
+        SparaMeddelande(filnamn);
+    }
+    else if (val == "2")
+    {
+        LäsaMeddelande(filnamn);
+    } else if (val == "3")
+    {
+        Console.WriteLine("Tack och hej!");
+        break;
+    }
+
+
+}
+/* ****************
+*** M E T O D E R **
+********************/
+
+
+/// <summary>
+/// Programen
+/// </summary>
+static void VisaPresentation()
+{
+    Console.WriteLine("""
+    =======================================
+    Program för att spara program på disken
+    =======================================
+    """);
+}
+
+/// <summary>
+/// Visar programets meny
+/// </summary>
+static void VisaMeny()
+{
+    Console.WriteLine("""
+
+    1. Spara Meddelande
+    2. Läsa Meddelande
+    3. Avsluta Programet
+    Vad väljer du?
+    """);
+    Console.WriteLine("Ange ditt val: ");
+}
+
+/// <summary>
+/// Sparar ett meddelande i en textfil
+/// </summary>
+static void SparaMeddelande(string fil)
+{
+    Console.WriteLine("Ange ditt Meddelande:");
+    string meddelande = Console.ReadLine();
+    File.WriteAllText("Text.medd", meddelande);
+    Console.WriteLine("Ditt meddelande har sparas ");
+}
+/// <summary>
+/// Läsrer in från textfilen och skriver meddelande
+/// </summary>
+static void LäsaMeddelande(string filin)
+{
+    string meddelande = File.ReadAllText("Text.medd");
+    Console.WriteLine("Ditt senaste meddelande:");
+    Console.WriteLine("---------------------");
+    Console.WriteLine(meddelande);
+    Console.WriteLine("---------------------");
+}
---
+++ Kapitel-6/Miniraknare/Program.cs
@@ -0,0 +1,81 @@
+﻿/*************
+*** M A I N ***
+**************/
+Console.Clear();
+
+VisaProgram();
+
+while (true)
+{
+    Meny();
+    string val = Console.ReadLine();
+    if (val == "1")
+    {
+        Addition();
+    }
+    else if (val == "2")
+    {
+        Subtraktion();
+    }else if (val == "3")
+    {
+        Multiplikation();
+    }else if (val == "4")
+    {
+        Divition();
+    }else if (val == "5")
+    {
+        Console.WriteLine("Hej då");
+        break;
+    }
+}
+
+/* ****************
+*** M E T O D E R **
+********************/
+static void VisaProgram()
+{
+    
+}
+
+static void Meny()
+{
+    Console.WriteLine("""
+    
+    1. Addition
+    2. Subtraktion
+    3. Multiplikation
+    4. Divition
+    5. Avsluta Programet
+    """);
+    Console.WriteLine("Ange ditt val: ");
+}
+
+static void Addition( )
+{
+    Console.WriteLine("Ange tal:");
+    int tal1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ange tal:");
+    int tal2 = int.Parse(Console.ReadLine());
+}
+
+static void Subtraktion()
+{
+    Console.WriteLine("Ange tal:");
+    int tal1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ange tal:");
+    int tal2 = int.Parse(Console.ReadLine());
+}
+static void Multiplikation()
+{
+    Console.WriteLine("Ange tal:");
+    int tal1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ange tal:");
+    int tal2 = int.Parse(Console.ReadLine());
+}
+static void Divition()
+{
+   Console.WriteLine("Ange tal:");
+    int tal1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ange tal:");
+    int tal2 = int.Parse(Console.ReadLine());
+}
